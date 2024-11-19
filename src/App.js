d0bd710afddcee40aa6bff68ecdce107a722cb42
mhainid <ayoubmhainid01@gmail.com>
2024-11-19 16:52:52 +0100

    findusers

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import { UserPost } from './UserPost'
+import React, { useState } from 'react'
 
 export const App = () => {
-  const [users,setUsers] = useState([])
+  const jsonArray =[
+    { nom:'mhainid',
+      prenom:'ayoub',
+      age:'19',
+      filiere:'dev'
+    },
+    { nom:'said',
+      prenom:'bolid',
+      age:'26',
+      filiere:'eco'
+    },
+    { nom:'hassan',
+      prenom:'melal',
+      age:'22',
+      filiere:'MEQ'
+    }
+  ]
+  const [data,setData]=useState(jsonArray)
+  const [value,setValue]=useState('');
 
-  useEffect (()=>{
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then((data)=>{
-      setUsers(data.data)
-    })
-},[])
-
-
+  const search = ()=>{
+    const newData=data.filter((student)=>student.prenom===value);
+    setData(newData)
+  }
   return (
     <div>
+      <label>filiere</label><input type='text' value={value} onChange={(e)=> setValue(e.target.value)}/><br></br>
+      <button onClick={()=>search()}>recherche</button>
       <table border={1}>
-        <tr><th>id</th><th>nom&prenom</th><th>username</th><th>email</th><th>postes</th></tr>
-        {
-          users && users.length && 
-          users.map((user)=> {
-            return <tr><td>{user.id}</td><td>{user.name}</td><td>{user.username}</td><td>{user.email}</td><td><Link to={`posts/${user.id}`}>detail</Link></td></tr>
-          })
-        }
+        <tr><th>nom</th><th>prenom</th><th>age</th><th>filiere</th></tr>
+        {data && data.length &&
+        data.map((student)=>{
+          return <tr>
+            <td>{student.nom}</td><td>{student.prenom}</td><td>{student.age}</td><td>{student.filiere}</td>
+          </tr>
+        })}
         
       </table>
     </div>
--- src/index.js
+++ src/index.js
@@ -7,21 +7,14 @@ import ReactDOM from 'react-dom/client'
 
 
 import { App } from './App';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import { UserPost } from './UserPost';
+
 
 
 const element = document.getElementById("root"); 
 const root = ReactDOM.createRoot(element); 
 root.render(
 <>
-<BrowserRouter>
-    <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/posts/:id' element={<UserPost />} />
-
-    </Routes>
-</BrowserRouter>
+<App />
 
 
 </>
